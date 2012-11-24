663f184ced7f7cecbe1f020188dea70e52e352d1
Alejandro Riera <ariera@gmail.com>
2012-11-24 23:58:02 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peehere::Application.config.secret_token = 'd2e77fead93833a0eda54885c5a28502c8f0aae06c753ba3b7803ad29b52b67826f78f45d669991f697e01f82aa3d4491bea820a84527b080134e2be47adfdac'

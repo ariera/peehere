f5174928a02bd71c86910fed9350b7ab69002533
Alejandro Riera <ariera@gmail.com>
2013-03-10 21:18:55 +0100

    bugfix star system

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -9,6 +9,22 @@ class Rating < ActiveRecord::Base
     return value if kind.to_s == 'overall'
 
     # this a bizarre way to convert percentage to 5 stars rating
-    5.times{|i| return (i+1).to_f if 1/5.to_f * (i+1) >= value }
+    #five_star_rating = 5.times{|i| return (i+1).to_f if 1/5.to_f * (i+1) >= value }
+    self.convert_to_five_star_rating(kind, value)
+  end
+
+  def self.convert_to_five_star_rating(kind, value)
+    # this a bizarre way to convert percentage to 5 stars rating
+    five_star_rating = 5.times{|i| return (i+1).to_f if 1/5.to_f * (i+1) >= value }
+    case kind.to_sym
+    when :pay, :wait, :crowded
+      self.invert_five_star_rating(five_star_rating)
+    else
+      five_star_rating
+    end
+  end
+
+  def self.invert_five_star_rating(five_star_rating)
+    6.0 - five_star_rating
   end
 end

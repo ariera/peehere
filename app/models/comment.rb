7b9c0b3442a0b6dd391d21e9bc0da888aaf98c8f
Alejandro Riera <ariera@gmail.com>
2013-03-16 18:21:33 +0100

    comentarios serializados en xml

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,13 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :location
   attr_accessible :body, :location_id, :user_id
+
+  def to_xml(options={})
+    options.merge!(:except => [:created_at, :updated_at, :id, :location_id, :user_id], :methods => :user_name)
+    super(options)
+  end
+
+  def user_name
+    user.name
+  end
 end
